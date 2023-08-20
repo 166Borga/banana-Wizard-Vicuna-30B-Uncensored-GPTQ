3861c6cfaca17f2291cdf5165082a4da6e0b2d21
166Borga <141344113+166Borga@users.noreply.github.com>
2023-08-20 06:15:33 +0300

    Update download.py

--- download.py
+++ download.py
@@ -5,8 +5,8 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    tokenizer = AutoTokenizer.from_pretrained("TheBloke/Wizard-Vicuna-30B-Uncensored-GPTQ")
-    model = AutoModelForCausalLM.from_pretrained("TheBloke/Wizard-Vicuna-30B-Uncensored-GPTQ")
+    tokenizer = AutoTokenizer.from_pretrained("ehartford/Wizard-Vicuna-30B-Uncensored")
+    model = AutoModelForCausalLM.from_pretrained("ehartford/Wizard-Vicuna-30B-Uncensored")
 
 if __name__ == "__main__":
     download_model()
